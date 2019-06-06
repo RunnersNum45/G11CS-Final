b6cb72d46db0c5c1e03fd2decfba81d1afb9d441
Ted <p_t@fastmail.net>
2019-06-05 23:39:36 -0400

    Made updates

--- mnist/Testing.py
+++ mnist/Testing.py
@@ -1,16 +1,8 @@
-'''Trains a simple convnet on the MNIST dataset.
-
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
 
 batch_size = 128
 num_classes = 10
@@ -35,15 +27,9 @@ img_rows, img_cols = 28, 28
 # # show the plot
 # plt.show()
 
-if K.image_data_format() == 'channels_first':
-	K.logging.set_verbosity(tf.logging.ERROR)
-	x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-	x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-	input_shape = (1, img_rows, img_cols)
-else:
-	x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-	x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-	input_shape = (img_rows, img_cols, 1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
