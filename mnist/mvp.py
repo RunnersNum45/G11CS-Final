f592db6512b8d909d35238a3ff13cc4bdf3728b0
Ted <p_t@fastmail.net>
2019-06-05 23:13:54 -0400

    Added testing file

--- mnist/mvp.py
+++ mnist/mvp.py
@@ -22,27 +22,28 @@ img_rows, img_cols = 28, 28
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-import matplotlib.pyplot as plt
-# plot 4 images as gray scale
-plt.subplot(221)
-plt.imshow(x_train[0], cmap=plt.get_cmap('gray'))
-plt.subplot(222)
-plt.imshow(x_train[1], cmap=plt.get_cmap('gray'))
-plt.subplot(223)
-plt.imshow(x_train[2], cmap=plt.get_cmap('gray'))
-plt.subplot(224)
-plt.imshow(x_train[3], cmap=plt.get_cmap('gray'))
-# show the plot
-plt.show()
+# import matplotlib.pyplot as plt
+# # plot 4 images as gray scale
+# plt.subplot(221)
+# plt.imshow(x_train[0], cmap=plt.get_cmap('grey'))
+# plt.subplot(222)
+# plt.imshow(x_train[1], cmap=plt.get_cmap('grey'))
+# plt.subplot(223)
+# plt.imshow(x_train[2], cmap=plt.get_cmap('grey'))
+# plt.subplot(224)
+# plt.imshow(x_train[3], cmap=plt.get_cmap('grey'))
+# # show the plot
+# plt.show()
 
 if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+	K.logging.set_verbosity(tf.logging.ERROR)
+	x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+	x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+	input_shape = (1, img_rows, img_cols)
 else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+	x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+	x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+	input_shape = (img_rows, img_cols, 1)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -58,32 +59,30 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
+				 activation='relu',
+				 input_shape=input_shape))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Dropout(0.25)) 
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dense(128, activation='relu')) 
+model.add(Dropout(0.5)) 
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+			  optimizer=keras.optimizers.Adadelta(),
+			  metrics=['accuracy'])
 
 model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+		  batch_size=batch_size,
+		  epochs=epochs,
+		  verbose=1,
+		  validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
-model_json = model.to_json()
+with open("MVP_Save.json", "w") as json_file:
+  json_file.write(model.to_json())
 
-with open("model_json", "w") as json_file:
-  json_file.write(model.json)
-
-model.save_weights("model.h5")
+model.save_weights("MVP_weights.h5")
